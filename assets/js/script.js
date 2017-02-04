a01d74a7d59b4846295c03d1cbb9bf387b3209f0
Yahya Hussein <yahyahussein91@gmail.com>
2017-02-03 22:40:41 -0800

    toggled done on and off

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,10 @@ $("li").click(function(){
 	//if li is grey
 	if($(this).css("color") === "rgb(128, 128, 128)"){
 		//turn it black
-		$(this).css("color", "black");
+		$(this).css({
+			color: "black",
+			textDecoration: "none",
+		});
 	}
 	else {
 		//turn it gray
