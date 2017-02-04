5f376b0d7676aae08fee94d429c21dd3ddbaa743
Yahya Hussein <yahyahussein91@gmail.com>
2017-02-03 22:42:20 -0800

    changed code to just toggle if completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,4 @@
 //Check off specif todos by clicking
 $("li").click(function(){
-	//if li is grey
-	if($(this).css("color") === "rgb(128, 128, 128)"){
-		//turn it black
-		$(this).css({
-			color: "black",
-			textDecoration: "none",
-		});
-	}
-	else {
-		//turn it gray
-		$(this).css({
-			color: "gray",
-			textDecoration: "line-through",
-		});
-	}
-
+	$(this).toggleClass("completed");
 });
