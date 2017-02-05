6d2858aebd19c5231b028306251c32f237eadc46
Yahya Hussein <yahyahussein91@gmail.com>
2017-02-04 16:36:46 -0800

    fixed input type error

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,12 +12,12 @@ $("ul").on("click", "span", function(event){
 	event.stopPropagation();
 });
 
-$("input=[type=text]").keypress(function(event){
+$("input[type='text']").keypress(function(event){
 	if(event.which === 13){
 		//grabbing new todo text from input box
 		var todoText = $(this).val();
 		$(this).val("");
 		//create new li and add to li
-		$("ul").append("<li><span>X</span " + todoText + "</li>");
+		$("ul").append("<li><span>X</span> " + todoText + "</li>");
 	}
 });
