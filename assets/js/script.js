99915466faadb2e0f589eca658b9f4da54036c70
Yahya Hussein <yahyahussein91@gmail.com>
2017-02-04 16:26:09 -0800

    code for extracting input

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,5 +12,8 @@ $("span").click(function(event){
 });
 
 $("input=[type=text]").keypress(function(event){
-	if(event.which === 13)
+	if(event.which === 13){
+		//extract text from input box
+		$(this).val();
+	}
 });
