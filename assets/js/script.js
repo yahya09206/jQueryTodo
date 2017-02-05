9b40c0cfa263e1b60f7424f2002d10bef856bb41
Yahya Hussein <yahyahussein91@gmail.com>
2017-02-04 16:29:44 -0800

    added span to new items that will be added

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,6 +17,6 @@ $("input=[type=text]").keypress(function(event){
 		var todoText = $(this).val();
 		$(this).val("");
 		//create new li and add to li
-		$("ul").append("<li>" + todoText + "</li>");
+		$("ul").append("<li><span>X</span " + todoText + "</li>");
 	}
 });
